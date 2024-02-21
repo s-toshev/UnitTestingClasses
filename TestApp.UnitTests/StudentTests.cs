ad854a879b6c8afacb4a7cc8709a9c9f168249bb
s-toshev <simeon.toshev@yahoo.bg>
2024-02-21 23:44:01 +0200

    21 Feb

--- TestApp.UnitTests/StudentTests.cs
+++ TestApp.UnitTests/StudentTests.cs
@@ -19,13 +19,17 @@ public class StudentTests
     public void Test_AddAndGetByCity_ReturnsStudentsInCity_WhenCityExists()
     {
         // Arrange
+
+
         string[] students = { "John Doe 25 Sofia", "Jane Smith 22 Varna", "Alice Johnson 20 Sofia" };
+        string wantedTown = "Sofia";
         string expected = $"John Doe is 25 years old.{Environment.NewLine}Alice Johnson is 20 years old.";
 
         // Act
+        string actual = this._student.AddAndGetByCity(students, wantedTown);
 
         // Assert
-        //Assert.That(result, Is.EqualTo(expected));
+        Assert.AreEqual(expected, actual);
     }
 
     [Test]
