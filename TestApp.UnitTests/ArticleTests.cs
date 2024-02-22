a30184d194e1e4dc5deb8c04c967f15ac1f6f795
s-toshev <simeon.toshev@yahoo.bg>
2024-02-22 19:59:35 +0200

    Feb 21

--- TestApp.UnitTests/ArticleTests.cs
+++ TestApp.UnitTests/ArticleTests.cs
@@ -1,37 +1,198 @@
 ﻿using NUnit.Framework;
-
 using System;
+using System.Collections.Generic;
 
-namespace TestApp.UnitTests;
-
-public class ArticleTests
+namespace TestApp.UnitTests
 {
-    // TODO: write the setup method
-
-    // TODO: finish test
-    [Test]
-    public void Test_AddArticles_ReturnsArticleWithCorrectData()
+    public class ArticleTests
     {
-        // Arrange
+        private Article _article;
 
-        // Act
+        [SetUp]
+        public void SetUp()
+        {
+            this._article = new Article();
+        }
 
-        // Assert
-        //Assert.That(result.ArticleList, Has.Count.EqualTo(3));
-        //Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
-        //Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
-        //Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+        // Tests for AddArticles Method
+        [Test]
+        public void AddSingleArticle()
+        {
+            // Arrange
+            string[] articles = { "Title1 Content1 Author1" };
+
+            // Act
+            Article result = this._article.AddArticles(articles);
+
+            // Assert
+            Assert.AreEqual(1, result.ArticleList.Count);
+        }
+
+        [Test]
+        public void AddMultipleArticles()
+        {
+            // Arrange
+            string[] articles = { "Title1 Content1 Author1", "Title2 Content2 Author2", "Title3 Content3 Author3" };
+
+            // Act
+            Article result = this._article.AddArticles(articles);
+
+            // Assert
+            Assert.AreEqual(3, result.ArticleList.Count);
+        }
+
+        [Test]
+        public void AddArticlesWithEmptyData()
+        {
+            // Arrange
+            string[] articles = { "   " };
+
+            // Act
+            Article result = this._article.AddArticles(articles);
+
+            // Assert
+            Assert.AreEqual(1, result.ArticleList.Count);
+        }
+
+        [Test]
+        public void AddArticlesWithNullData()
+        {
+            // Arrange
+            string[] articles = { null };
+
+            // Act
+
+            // Assert
+
+            Assert.Throws<NullReferenceException>(()=> this._article.AddArticles(articles));
+        }
+
+        [Test]
+        public void AddArticlesWithSpecialCharacters()
+        {
+            // Arrange
+            string[] articles = { "!@#$ %^& *() <>? :; Title1 Content1 Author1" };
+
+            // Act
+            Article result = this._article.AddArticles(articles);
+
+            // Assert
+            Assert.AreEqual(1, result.ArticleList.Count);
+        }
+
+        // Tests for GetArticleList Method
+        [Test]
+        public void GetArticleListByTitleAscending()
+        {
+            // Arrange
+            Article article = new Article();
+            article.AddArticles(new string[] { "C ContentC AuthorC", "B ContentB AuthorB", "A ContentA AuthorA" });
+
+            // Act
+            string result = article.GetArticleList(article, "title");
+
+            // Assert
+            Assert.AreEqual("A - ContentA: AuthorA\nB - ContentB: AuthorB\nC - ContentC: AuthorC", result);
+        }
+
+        [Test]
+        public void GetArticleListByContentAscending()
+        {
+            // Arrange
+            Article article = new Article();
+            article.AddArticles(new string[] { "TitleC ContentC AuthorC", "TitleB ContentC AuthorB", "TitleA ContentA AuthorA" });
+
+            // Act
+            string result = article.GetArticleList(article, "content");
+
+            // Assert
+            Assert.AreEqual("TitleA - ContentA: AuthorA\nTitleB - ContentB: AuthorB\nTitleC - ContentC: AuthorC", result);
+        }
+
+        [Test]
+        public void GetArticleListByAuthorAscending()
+        {
+            // Arrange
+            Article article = new Article();
+            article.AddArticles(new string[] { "TitleC ContentC AuthorC", "TitleB ContentB AuthorB", "TitleA ContentA AuthorA" });
+
+            // Act
+            string result = article.GetArticleList(article, "author");
+
+            // Assert
+            Assert.AreEqual("TitleA ContentA AuthorA - ContentA: AuthorA\nTitleB ContentB AuthorB - ContentB: AuthorB\nTitleC ContentC AuthorC - ContentC: AuthorC", result);
+        }
+
+
+        [Test]
+        public void GetArticleListByInvalidCriteria_ReturnsEmptyString()
+        {
+            // Arrange
+            Article article = new Article();
+            article.AddArticles(new string[] { "Title1 Content1 Author1", "Title2 Content2 Author2", "Title3 Content3 Author3" });
+
+            // Act
+            string result = article.GetArticleList(article, "invalid");
+
+            // Assert
+            Assert.AreEqual("", result);
+        }
+
+        [Test]
+        public void GetArticleListWithEmptyArticleList_ReturnsEmptyString()
+        {
+            // Arrange
+            Article article = new Article();
+
+            // Act
+            string result = article.GetArticleList(article, "title");
+
+            // Assert
+            Assert.AreEqual("", result);
+        }
+
+        [Test]
+        public void GetArticleListWithNullArticleList_ReturnsEmptyString()
+        {
+            // Arrange
+            Article article = new Article();
+            article.ArticleList = null;
+
+            // Act
+
+            // Assert
+            Assert.Throws<ArgumentNullException>(()=> article.GetArticleList(article,"title"));
+        }
+
+        [Test]
+        public void GetArticleListWithEmptyCriteria_ReturnsEmptyString()
+        {
+            // Arrange
+            Article article = new Article();
+            article.AddArticles(new string[] { "Title1 Content1 Author1", "Title2 Content2 Author2", "Title3 Content3 Author3" });
+
+            // Act
+            string result = article.GetArticleList(article, "");
+
+            // Assert
+            Assert.AreEqual("", result);
+        }
+
+        [Test]
+        public void GetArticleListWithNullCriteria_ReturnsEmptyString()
+        {
+            // Arrange
+            Article article = new Article();
+            article.AddArticles(new string[] { "Title1 Content1 Author1", "Title2 Content2 Author2", "Title3 Content3 Author3" });
+
+            // Act
+            string result = article.GetArticleList(article, null);
+
+            // Assert
+            Assert.AreEqual("", result);
+        }
+
     }
 
-    [Test]
-    public void Test_GetArticleList_SortsArticlesByTitle()
-    {
-        // TODO: finish test
-    }
 
-    [Test]
-    public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
-    {
-        // TODO: finish test
-    }
 }
--- TestApp.UnitTests/PlanetTests.cs
+++ TestApp.UnitTests/PlanetTests.cs
@@ -1,28 +1,39 @@
 ﻿using NUnit.Framework;
-
 using System;
 
-namespace TestApp.UnitTests;
-
-public class PlanetTests
+namespace TestApp.UnitTests
 {
-    // TODO: finish test
-    [Test]
-    public void Test_CalculateGravity_ReturnsCorrectCalculation()
+    public class PlanetTests
     {
-        // Arrange
-        //Planet earth = new("Earth", 12742, 149600000, 1);
-        double mass = 1000;
-        //double expectedGravity = mass * 6.67430e-11 / Math.Pow(earth.Diameter / 2, 2);
+        [Test]
+        public void Test_CalculateGravity_ReturnsCorrectCalculation()
+        {
+            // Arrange
+            Planet earth = new("Earth", 12742, 149600000, 1);
+            double mass = 1000;
+            double expectedGravity = mass * 6.67430e-11 / Math.Pow(earth.Diameter / 2, 2);
 
-        // Act
+            // Act
+            double actualGravity = earth.CalculateGravity(mass);
 
-        // Assert
-    }
+            // Assert
+            Assert.AreEqual(expectedGravity, actualGravity);
+        }
 
-    [Test]
-    public void Test_GetPlanetInfo_ReturnsCorrectInfo()
-    {
-        // TODO: finish test
+        [Test]
+        public void Test_GetPlanetInfo_ReturnsCorrectInfo()
+        {
+            // Arrange
+            Planet mars = new("Mars", 6779, 227900000, 2);
+
+            // Act
+            string info = mars.GetPlanetInfo();
+
+            // Assert
+            Assert.AreEqual("Planet: Mars\r\nDiameter: 6779 km\r\nDistance from the Sun: 227900000 km\r\nNumber of Moons: 2", info);
+        }
+
+
+
     }
 }
