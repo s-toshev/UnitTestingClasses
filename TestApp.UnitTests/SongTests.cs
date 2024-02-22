3ee1071ba72a1ecf81de6a5ebb0c632535942324
s-toshev <simeon.toshev@yahoo.bg>
2024-02-22 09:12:10 +0200

    22 Feb

--- TestApp.UnitTests/SongTests.cs
+++ TestApp.UnitTests/SongTests.cs
@@ -20,23 +20,140 @@ public class SongTests
     {
         // Arrange
         string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string wantedList = "All";
         string expected = $"Song1{Environment.NewLine}Song2{Environment.NewLine}Song3";
 
         // Act
-
+        string actual = this._song.AddAndListSongs(songs, wantedList);
         // Assert
-        //Assert.That(result, Is.EqualTo(expected));
+        Assert.AreEqual(expected, actual);
     }
 
     [Test]
     public void Test_AddAndListSongs_ReturnsFilteredSongs_WhenWantedListIsSpecific()
     {
-        // TODO: finish test
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string wantedList = "Pop";
+        string expected = $"Song1{Environment.NewLine}Song3";
+
+        // Act
+        string actual = this._song.AddAndListSongs(songs, wantedList);
+        // Assert
+        Assert.AreEqual(expected, actual);
     }
 
     [Test]
     public void Test_AddAndListSongs_ReturnsEmptyString_WhenNoSongsMatchWantedList()
     {
-        // TODO: finish test
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string wantedList = "InTheMix";
+        string expected = "";
+
+        // Act
+        string actual = this._song.AddAndListSongs(songs, wantedList);
+        // Assert
+        Assert.AreEqual(expected, actual);
     }
+
+    [Test]
+    public void Test_AddAndListSongs_ThrowsIndexOutOfRangeExceptionWhenTimeMissingFromSong()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1", "Rock_Song2", "Pop_Song3" };
+        string wantedList = "Pop";
+
+        // Act
+        // Assert
+        Assert.Throws<IndexOutOfRangeException>(()=> this._song.AddAndListSongs(songs, wantedList));
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsEmptyString_WhenWantedListIsEmpty()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string wantedList = string.Empty;
+        string expected = "";
+
+        // Act
+        string actual = this._song.AddAndListSongs(songs, wantedList);
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsEmptyString_WhenWantedListIsNull()
+    {
+        // Arrange
+        string[] songs = { "Pop_Song1_3:30", "Rock_Song2_4:15", "Pop_Song3_3:00" };
+        string wantedList = null;
+        string expected = "";
+
+        // Act
+        string actual = this._song.AddAndListSongs(songs, wantedList);
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ReturnsEmptyString_WhenSongsInputIsEmptyArray()
+    {
+        // Arrange
+        string[] songs = Array.Empty<string>();
+
+        string wantedList = "Pop";
+        string expected = "";
+
+        // Act
+        string actual = this._song.AddAndListSongs(songs, wantedList);
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ShouldntReturnSongs_WhenSongsInputIsEmptySeparatedStrings()
+    {
+        // Arrange
+        string[] songs = {" _  _ ", " _  _    ", " _    _   " };
+
+        string wantedList = "all";
+        string expected = "";
+
+        // Act
+        string actual = this._song.AddAndListSongs(songs, wantedList);
+        // Assert
+        Assert.AreEqual(expected, actual);
+        Assert.IsNotEmpty(actual);
+    }
+
+    [Test]
+    public void Test_AddAndListSongs_ShouldntReturnSongs_WhenSongsInputIsEmptyStrings()
+    {
+        // Arrange
+        string[] songs = { "    ", "      ", "       " };
+
+        string wantedList = "all";
+
+        // Act
+      
+        // Assert
+        Assert.Throws<IndexOutOfRangeException>(() => this._song.AddAndListSongs(songs, wantedList));
+    }
+
+
+    [Test]
+    public void Test_AddAndListSongs_ThrowsNullReferenceExceptionWhenInputIsNull()
+    {
+        // Arrange
+        string[] songs = {null,null,null };
+
+        string wantedList = "Pop";
+
+        // Act
+        // Assert
+        Assert.Throws<NullReferenceException>(() => this._song.AddAndListSongs(songs, wantedList));
+    }
+
 }
--- TestApp.UnitTests/StudentTests.cs
+++ TestApp.UnitTests/StudentTests.cs
@@ -19,8 +19,6 @@ public class StudentTests
     public void Test_AddAndGetByCity_ReturnsStudentsInCity_WhenCityExists()
     {
         // Arrange
-
-
         string[] students = { "John Doe 25 Sofia", "Jane Smith 22 Varna", "Alice Johnson 20 Sofia" };
         string wantedTown = "Sofia";
         string expected = $"John Doe is 25 years old.{Environment.NewLine}Alice Johnson is 20 years old.";
@@ -33,14 +31,110 @@ public class StudentTests
     }
 
     [Test]
+    public void Test_AddAndGetByCity_ReturnsCorrectStudentInfo_WithExtremeAgeValues()
+    {
+        // Arrange
+        string[] students = { "John Doe 215 Sofia", "Jane Smith 242 Varna", "Alice Johnson 120 Sofia" };
+        string wantedTown = "Sofia";
+        string expected = $"John Doe is 215 years old.{Environment.NewLine}Alice Johnson is 120 years old.";
+
+        // Act
+        string actual = this._student.AddAndGetByCity(students, wantedTown);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void Test_AddAndGetByCity_ReturnsStudentsInCity_WhenMultipleStudentsInTheSameCity()
+    {
+        // Arrange
+        string[] students = { "John Doe 25 Sofia", "Jane Smith 22 Sofia", "Alice Johnson 20 Sofia", "Naomi Jameson 44 Varna", "Versachi Ivanov 13 Sofia" };
+        string wantedTown = "Sofia";
+        string expected = "John Doe is 25 years old.\r\nJane Smith is 22 years old.\r\nAlice Johnson is 20 years old.\r\nVersachi Ivanov is 13 years old.";
+
+        // Act
+        string actual = this._student.AddAndGetByCity(students, wantedTown);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+
+    [Test]
+    public void Test_AddAndGetByCity_ReturnsCorrectStudentsInCity_WhenStudentsWithTheSameNameAgeDifferentCity()
+    {
+        // Arrange
+        string[] students = { "John Doe 25 Sofia", "Jon Doe 25 Varna", "John Doe 25 Pleven" };
+        string wantedTown = "Pleven";
+        string expected = "John Doe is 25 years old.";
+
+        // Act
+        string actual = this._student.AddAndGetByCity(students, wantedTown);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+
+    [Test]
+    public void Test_AddAndGetByCity_ThrowsFormatException_WhenInputIsEmptySpace()
+    {
+        // Arrange
+        string[] students = { "  ", "   ", "   " };
+        string wantedTown = "   ";
+
+
+
+        // Assert
+        Assert.Throws<FormatException>(() => this._student.AddAndGetByCity(students, wantedTown));
+    }
+
+
+    [Test]
+    public void Test_AddAndGetByCity_ThrowsFormatException_WhenInputIsNull()
+    {
+        // Arrange
+        string[] students = { null, null,null };
+
+        string wantedTown = null;
+
+
+
+        // Assert
+        Assert.Throws<NullReferenceException>(() => this._student.AddAndGetByCity(students, wantedTown));
+    }
+
+
+
+    [Test]
     public void Test_AddAndGetByCity_ReturnsEmptyString_WhenCityDoesNotExist()
     {
-        // TODO: finish test
+        // Arrange
+        string[] students = { "John Doe 25 Sofia", "Jane Smith 22 Varna", "Alice Johnson 20 Sofia" };
+        string wantedTown = "ThisCityDoesNotExist";
+        string expected = "";
+
+        // Act
+        string actual = this._student.AddAndGetByCity(students, wantedTown);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
     }
 
     [Test]
     public void Test_AddAndGetByCity_ReturnsEmptyString_WhenNoStudentsGiven()
     {
-        // TODO: finish test
+        // Arrange
+        string[] students = Array.Empty<string>();
+
+        string wantedTown = "Sofia";
+        string expected = "";
+
+        // Act
+        string actual = this._student.AddAndGetByCity(students, wantedTown);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
     }
 }
